7a1513a0290d912804edd4a0b254cf6bf86ea206
Yuga Yamamoto <srise1103@gmail.com>
2017-05-07 16:27:22 +0900

    自動的に飛ぶバーの追加

---
+++ Assets/ControllBer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllBer : MonoBehaviour {
+    public static float cnt;
+    public static float stopCnt;
+
+    float value;
+    public static bool b;
+
+    Slider _slider;
+	// Use this for initialization
+	void Start () {
+        _slider = GameObject.Find("Slider").GetComponent<Slider>();
+        b = false;
+        cnt = 0;
+        stopCnt = Random.Range(5, 10) * 60;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        value = cnt / stopCnt;
+        _slider.value = value;
+
+        if(cnt == stopCnt)
+        {
+            b = true;
+        }
+        if(cnt == 0)
+        {
+            _slider.value = _slider.minValue;
+        }
+
+        cnt++;
+	}
+}
--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 
 public class PlayerMove : MonoBehaviour {
@@ -13,22 +13,27 @@ public class PlayerMove : MonoBehaviour {
     //スピード
     Vector2 spd = Vector3.zero;
     //移動してるかどうか
-    bool checkMove;
+    public static bool checkMove;
+    
+    
 
     Rigidbody2D rb;
 
     // Use this for initialization
-	void Start () {
+    void Start() {
         checkMove = false;
+        
 
         rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-        if(transform.position.y < 0)
+        if (transform.position.y < 0)
         {
+            //地面を越そうとしたら地面の上に移動させる
             transform.position = new Vector3(transform.position.x, 0, transform.position.z);
         }
 
@@ -43,26 +48,45 @@ public class PlayerMove : MonoBehaviour {
             //ボタンを離したとき
             if (Input.GetMouseButtonUp(0))
             {
+                //離したときのマウスの座標を記録
                 endPos = Input.mousePosition;
+                //もうおせないようにする
                 checkMove = true;
 
-                spd = (endPos - startPos) * -2;
+                //速度の設定
+                spd.x = (endPos.x - startPos.x) * -2.0f / 60.0f;
+                spd.y = (endPos.y - startPos.y) * -2.0f / 60.0f;
 
-                spd /= 30;
-
-                if(spd.x < 0)
+                if(spd.x < spd.y)
                 {
+                    spd.y = spd.x * 0.75f;
+                }
+                
+
+                //後ろに飛んで行きそうなら
+                if (spd.x < 0)
+                {
+                    //もう一回飛ばそうとする
                     checkMove = false;
                 }
+
             }
+
+            if (ControllBer.b)
+            {
+                //速度の設定
+                spd.x = 10.0f;
+                spd.y = 10.0f;
+
+                checkMove = true;
+            }
+
         }
-        if (checkMove == true)
+        else
         {
+            spd.y -= rb.gravityScale;
+
             rb.velocity = spd;
-
-            // transform.Translate(spd);
-            //transform.Translate(0, (-9.8f / 60.0f / 10.0f), 0);
-
 
             spd *= 0.98f;
         }
